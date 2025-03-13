fa55b6045a026820d8b74db942a760f2a1eeaa43
Areg <areg.sarkisyan.99@mail.ru>
2025-03-13 07:08:22 +0300

    previous calc func

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -1,36 +1,67 @@
 import CalculatorBtn from "../CalculatorBtn/CalculatorBtn.jsx";
 import "./calculator.css";
-import {useState} from "react";
+import { useState } from "react";
 export default function Calculator({ loggedIn }) {
   const [currentNum, setNum] = useState(0);
-  console.log(currentNum);
+  const [operation, setOperation] = useState("");
+  const [lastNum, setLastNum] = useState(0);
+  const [res, setRes] = useState(0);
+
+  function sum() {
+    setRes(currentNum + lastNum);
+  }
+  function sub() {
+    setRes(lastNum - currentNum);
+  }
+  function div() {
+    setRes(lastNum / currentNum);
+  }
+  function multiply() {
+    setRes(lastNum * currentNum);
+  }
+
   return (
     <div className={`calculator ${loggedIn && "calculator_active"}`}>
-      <p className="calculator__exercise">{currentNum}</p>
-      <h2 className="calculator__res">=12,454</h2>
+      <p className="calculator__exercise">{lastNum ? lastNum + operation : ''}{currentNum}</p>
+      <h2 className="calculator__res">{res}</h2>
       <form action="" className="calculator__btns">
-      <CalculatorBtn text={"e"} type={'small'}/>
-      <CalculatorBtn text={"μ"} type={'small'}/>
-      <CalculatorBtn text={"sin"} type={'small'}/>
-      <CalculatorBtn text={"deg"} type={'small'}/>
-      <CalculatorBtn text={"Ac"} />
-      <CalculatorBtn text={"C"} setNum={()=>setNum(0)}/>
-      <CalculatorBtn text={"/"} />
-      <CalculatorBtn text={"*"} />
+        <CalculatorBtn text={"e"} type={"small"} />
+        <CalculatorBtn text={"μ"} type={"small"} />
+        <CalculatorBtn text={"sin"} type={"small"} />
+        <CalculatorBtn text={"deg"} type={"small"} />
+        <CalculatorBtn text={"AC"} />
+        <CalculatorBtn text={"C"} func={() => setNum(0)} />
+        <CalculatorBtn text={"/"} func={()=> setOperation('/')}/>
+        <CalculatorBtn text={"*"} func={()=> setOperation('*')}/>
         <div className="calculator__nums">
           {(function () {
             let result = [];
             for (let i = 9; i >= 0; i--) {
-              result.push(<CalculatorBtn key={i} text={i} setNum={()=>setNum(parseInt(currentNum.toString() + i))} currentNum={currentNum} />);
+              result.push(
+                <CalculatorBtn
+                  key={i}
+                  text={i}
+                  func={() => setNum(parseInt(currentNum.toString() + i))}
+                />
+              );
             }
             return result;
           })()}
           <CalculatorBtn text={"."} />
         </div>
         <div className="calculator__side-nums">
-          <CalculatorBtn text={"-"} />
-          <CalculatorBtn text={"+"} />
-          <CalculatorBtn text={"="} />
+          <CalculatorBtn text={"-"} func={()=> setOperation('-')}/>
+          <CalculatorBtn text={"+"} func={()=> setOperation('+')}/>
+          <CalculatorBtn text={"="} func={()=>{
+            switch(operation){
+              case '+':
+                sum();
+                break;
+              case '-':
+                sub();
+                break;
+            }
+          }}/>
         </div>
       </form>
     </div>
--- src/CalculatorBtn/CalculatorBtn.jsx
+++ src/CalculatorBtn/CalculatorBtn.jsx
@@ -1,23 +1,24 @@
-import './calculator__btn.css'
+import "./calculator__btn.css";
 
-export default function CalculatorBtn ({text, type, setNum, currentNum, click}) {
-  return(
-    <button className={`calculator__btn ${(
-      text === "/" ? 'calculator__btn_blue'
-      :
-      text === "*" ? 'calculator__btn_blue'
-      :
-      text === "-" ? 'calculator__btn_blue'
-      :
-      text === "+" ? 'calculator__btn_blue'
-      :
-      text === "=" && 'calculator__btn_blue'
-    )}
-    ${
-      type === "small" && 'calculator__btn_small'
-    }`
-    }
-    type={'button'}
-    onClick={()=>setNum()}>{text}</button>
-  )
+export default function CalculatorBtn({ text, type, func }) {
+  return (
+    <button
+      className={`calculator__btn ${
+        text === "/"
+          ? "calculator__btn_blue"
+          : text === "*"
+          ? "calculator__btn_blue"
+          : text === "-"
+          ? "calculator__btn_blue"
+          : text === "+"
+          ? "calculator__btn_blue"
+          : text === "=" && "calculator__btn_blue"
+      }
+    ${type === "small" && "calculator__btn_small"}`}
+      type={"button"}
+      onClick={() => func()}
+    >
+      {text}
+    </button>
+  );
 }
